63087b3974df86f14120f1bfdc9444ee80d0abeb
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-09 15:14:06 +0530

    Solved Q2 - Sieve of Eratosthenes | Mark multiples, print remaining

---
+++ Numbers/Q2_SieveOfEratosthenes.java
@@ -0,0 +1,28 @@
+class Solution{
+static void prime_Check_till_N(int n){
+
+boolean [] arr=new boolean [n+1];
+Arrays.fill(arr,true);
+arr[0]=false;
+arr[1]=false;
+for(int i=2;i<arr.length; i++){
+if(arr[i]==true){
+for(int j=i*i; j<arr.length; j+=i){
+arr[j]=false;
+}
+}
+} 
+for(int i=2;i<arr.length; i++){
+if(arr[i]==true){
+System.out.println(i+” “);
+}
+}
+}
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+
+prime_Check_till_N(n);
+}
+}
+
