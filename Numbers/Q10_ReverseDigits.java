a2dd8cf7a032aa99abcdb897085748115fa014c3
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-09 17:17:51 +0530

    Solved Q10 - Reverse Digits | rev=rev*10+rem trick

---
+++ Numbers/Q10_ReverseDigits.java
@@ -0,0 +1,24 @@
+class Solution{
+static  void reverse_digits(int n){
+int temp=n;
+int rev=0;
+while(temp>0){
+int rem=temp%10;
+rev=rev*10+rem;
+temp=temp/10;
+}
+System.out.println(rev);
+}
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+reverse_digits(n);
+
+}
+}
+	
+
+
+
+
+
