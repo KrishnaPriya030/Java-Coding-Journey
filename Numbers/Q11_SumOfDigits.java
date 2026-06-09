eb06f2601c1d243862ed1c206262eb1b73e4c27b
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-09 17:21:42 +0530

    Solved Q11 - Sum of Digits | rem=n%10 trick

---
+++ Numbers/Q11_SumOfDigits.java
@@ -0,0 +1,22 @@
+class Solution{
+static  void sum_digits(int n){
+int sum=0;
+while(n>0){
+int rem=n%10;
+sum+=rem;
+n=n/10;
+}
+System.out.println(sum);
+}
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+sum_digits(n);
+
+}
+}
+	
+
+
+
+
