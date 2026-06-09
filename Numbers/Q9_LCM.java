bcc9c474a5afb8879db9aa5381f97a10543d902c
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-09 17:12:30 +0530

    Solved Q9 - LCM | (a*b)/GCD formula, save originals before loop

---
+++ Numbers/Q9_LCM.java
@@ -0,0 +1,27 @@
+class Solution{
+static  void lcm(int a,int b){
+int org_a=a;
+int org_b=b;
+
+while(b!=0){
+int temp=b;
+b=a%b;
+a=temp;
+}
+int gcd=a;
+int lcm=( org_a * org_b)/gcd ;
+System.out.println(lcm);
+
+}
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+lcm(a,b);
+}
+}
+	
+
+
+
+
