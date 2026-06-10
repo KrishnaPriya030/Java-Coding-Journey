ee58d038da22cea2120a142966807a91475f68b1
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-10 19:28:31 +0530

    Solved Q19 - Automorphic Number | Compare digits of number with square

---
+++ Numbers/Q19_AutomorphicNumber.java
@@ -0,0 +1,24 @@
+class Solution{
+
+static void Automorphic_number(int n){
+int sqr=n*n;
+int temp=n;
+while(temp!=0){
+
+if(temp%10!=sqr%10){
+System.out.println("Not amorphic");
+return;
+}
+temp=temp/10;
+sqr=sqr/10;
+}
+System.out.println("amorphic");
+}
+}
+
+public static void main(Strig [] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+Automorphic_number(n);
+}
+}
