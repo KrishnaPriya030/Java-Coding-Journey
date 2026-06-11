75b184cfe28387c6c022cb0f4da5626304cf152e
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-11 18:50:56 +0530

    Solved Q30 - Octal To Decimal | Powers of 8 and digit extraction

---
+++ Numbers/Q30_OctalToDecimal.java
@@ -0,0 +1,23 @@
+class Solution{
+
+static void binary_to_decimal(int n){
+
+int power=0;
+int result=0;
+while(n>0){
+int rem=n%10;
+result+=rem * (int)Math.pow(8,power);
+power++;
+n=n/10;
+}
+System.out.println(result);
+}
+public static void main(String [] args){
+
+Scanner sc=new Scanner(System.in);
+
+int n = sc.nextInt();
+
+binary_to_decimal(n);
+}
+}
