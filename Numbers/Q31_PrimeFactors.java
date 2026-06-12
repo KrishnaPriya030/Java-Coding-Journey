bd35e6d129ed26e9d3469f214b392fc55590d14c
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-12 13:42:31 +0530

    Solved Q31 - Prime Factors | Prime factorization using division method

---
+++ Numbers/Q31_PrimeFactors.java
@@ -0,0 +1,28 @@
+class Solution{
+
+static void prime_factors(int n){
+
+if(n<=0){
+return;
+}
+
+for(int i=2 ; i*i<=n; i++){
+while(n%i==0){
+System.out.println(i);
+n=n/i;
+}
+}
+if(n>1){
+System.out.println(n);
+}
+}
+
+public static void main(String [] args){
+
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+
+
+prime_factors(n);
+}
+}
