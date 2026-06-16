01ab810fa0ac7a9f396e7e78b7c8f78eeb794528
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-16 22:52:44 +0530

    Update Q3_Factorial.java

--- Numbers/Q3_Factorial.java
+++ Numbers/Q3_Factorial.java
@@ -1,14 +1,22 @@
-Class Solution{
-static long fact(long n){
-if(n==0)
-return 1;
-return n*fact(n-1);
-}
-public static void main(String[] args){
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-System.out.println(fact(n));
-}
-}
+import java.util.Scanner;
 
+class Solution {
 
+    static long fact(long n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(fact(n));
+
+        sc.close();
+    }
+}
