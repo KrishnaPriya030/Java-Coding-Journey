234f32ac0f1d5baf3f2898363b73e521cad389cb
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-16 21:48:48 +0530

    Solved Q46 - Decimal to Octal | Same prepend pattern as Q45

---
+++ Numbers/Q46_DecimalToOctal.java
@@ -0,0 +1,24 @@
+class Solution{
+
+static void decimal_octal(int n){
+
+String oct="";
+if(n==0){
+System.out.println("0");
+return;
+}
+while(n>0){
+int rem=n%8;
+oct=rem+oct;
+n=n/8;
+}
+
+System.out.println(oct);
+}
+public static void main(String [] args){
+
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+decimal_octal(n);
+}
+}
