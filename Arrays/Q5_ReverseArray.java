7adcbae4e0e43315b4c3cfeff7c231e4ae4490b5
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-17 15:38:31 +0530

    Solved Array Q5 - Reverse Array | Two pointer swap approach

---
+++ Arrays/Q5_ReverseArray.java
@@ -0,0 +1,71 @@
+#APPROACH 1
+
+
+import java.util.Scanner;
+class Solution{
+
+static void reverse_array(int [] arr){
+
+for(int i=arr.length-1; i>=0; i--){
+
+System.out.print(arr[i]+" ");
+}
+}
+public static void main(String [] args){
+
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int [] arr=new int [n];
+	
+
+for(int i=0; i<arr.length; i++){
+
+	arr[i]=sc.nextInt();
+
+}
+ reverse_array(arr);
+}
+
+
+#Approach 2
+
+import java.util.Scanner;
+class Solution{
+
+static void reverse_array(int [] arr){
+
+int left=0;
+int right=arr.length-1;
+while(left<right){
+
+int temp=arr[left];
+arr[left]=arr[right];
+arr[right]=temp;
+
+left++;
+right--;
+}
+  // Step 2 — print normally left to right
+    for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+    }
+}
+public static void main(String [] args){
+
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int [] arr=new int [n];
+	
+
+for(int i=0; i<arr.length; i++){
+
+	arr[i]=sc.nextInt();
+
+}
+reverse_array(arr);
+}
+}
+
+
+}
+
