5a8e461e93dc4e9a9a96c4604dc38f35379d83e5
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-20 20:18:42 +0530

    Update Q14_SumOfElements.java

--- Arrays/Q14_SumOfElements.java
+++ Arrays/Q14_SumOfElements.java
@@ -1,23 +1,29 @@
 import java.util.*;
-class Solution{
 
-    static int sum(int [] arr){
+class Solution {
 
-int sum=0;
-for(int i=0; i<arr.length; i++){
-   sum+=arr[i];
-}
-return sum;
-    }
-    public static void main(String [] args){
+    static double average(int[] arr) {
 
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int [] arr=new int[n];
+        int sum = 0;
 
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
-       System.out.println(sum(arr));
+
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(average(arr));
     }
 }
