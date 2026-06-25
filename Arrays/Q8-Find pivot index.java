c9c32ac6602120d6150c87cb1a282fbdf71d6d9a
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-25 14:40:06 +0530

    Update Q8-Find pivot index.java

--- Arrays/Q8-Find pivot index.java
+++ Arrays/Q8-Find pivot index.java
@@ -2,17 +2,17 @@
     class Solution {
         public int pivotIndex(int[] nums) {
             
-            int total=0;
+            int rightsum=0;
 
             for(int i=0; i<nums.length; i++){
 
-                total+=nums[i];
+                rightsum+=nums[i];
             }
             int leftsum=0;
             for(int i=0; i<nums.length; i++){
 
                 
-                int rightsum=total-leftsum-nums[i];
+                rightsum-=nums[i];
 
                 if(leftsum==rightsum){
 
