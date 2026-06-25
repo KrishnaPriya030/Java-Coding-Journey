011cf016bd38af619a9b1ed4ce815470fe4e2ea3
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-26 00:44:09 +0530

    Update Qn9-Merge sorted array.java

--- Arrays/Qn9-Merge sorted array.java
+++ Arrays/Qn9-Merge sorted array.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
+
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int p1=m-1;
-        int p2=n-1;
-        int p3=m+n-1;
-     while (p2 >= 0){
 
-            if(p1>=0 && nums1[p1]>nums2[p2]){
+    static void merge_sorted_array(int[] nums1, int[] nums2, int m, int n) {
 
-                nums1[p3]=nums1[p1];
-                p1--;
-                p3--;
-            }
-            else{
-                nums1[p3]=nums2[p2];
-                p2--;
-                p3--;
-            }
-           
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[] nums1 = new int[m + n];
+        int[] nums2 = new int[n];
+
+        for (int i = 0; i < m + n; i++) {
+            nums1[i] = sc.nextInt();
         }
 
+        for (int i = 0; i < n; i++) {
+            nums2[i] = sc.nextInt();
+        }
+
+        merge_sorted_array(nums1, nums2, m, n);
+
+        for (int i = 0; i < m + n; i++) {
+            System.out.print(nums1[i] + " ");
+        }
     }
+}
