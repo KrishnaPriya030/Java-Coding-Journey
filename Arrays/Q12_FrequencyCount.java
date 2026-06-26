512d2c5ddc98ac7dd55beb592d23590229c6b330
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-26 16:02:00 +0530

    Update Q12_FrequencyCount.java

--- Arrays/Q12_FrequencyCount.java
+++ Arrays/Q12_FrequencyCount.java
@@ -51,3 +51,83 @@ class Solution {
         System.out.println(firstNonRepeating(nums));
     }
 }
+
+3.First repeating element
+    import java.util.*;
+
+class Solution {
+
+    static int firstRepeating(int[] nums) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums) {
+            if (freq.get(num) > 1) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
+4.Element with maximum frequency 
+
+    import java.util.*;
+
+class Solution {
+
+    static int maxFrequency(int[] nums) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int max = 0;
+        int ans = -1;
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                ans = entry.getKey();
+            }
+        }
+
+        return ans;
+    }
+}
+
+4.Elmnet with minimum frequency
+
+    import java.util.*;
+
+class Solution {
+
+    static int minFrequency(int[] nums) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int min = Integer.MAX_VALUE;
+        int ans = -1;
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+
+            if (entry.getValue() < min) {
+                min = entry.getValue();
+                ans = entry.getKey();
+            }
+        }
+
+        return ans;
+    }
+}
