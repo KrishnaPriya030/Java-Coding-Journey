7efd4de8cdeecc3b4c58ebaaf5e19be87b3248de
KrishnaPriya030 <krishnapriyas030@gmail.com>
2026-06-26 15:52:54 +0530

    Update Q12_FrequencyCount.java

--- Arrays/Q12_FrequencyCount.java
+++ Arrays/Q12_FrequencyCount.java
@@ -19,3 +19,35 @@ class Solution {
       
 }
 }
+find dirst non repeat  element
+
+import java.util.*;
+
+class Solution {
+
+    static int firstNonRepeating(int[] nums) {
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        // Count frequency
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // Find first non-repeating element
+        for (int num : nums) {
+            if (freq.get(num) == 1) {
+                return num;
+            }
+        }
+
+        return -1; // No non-repeating element
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 4, 2, 3, 5};
+
+        System.out.println(firstNonRepeating(nums));
+    }
+}
